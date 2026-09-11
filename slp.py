e824ca1f24907c8ee4cbd76456ef9d3c919c7026
yusufimantaka <imantakayusufbastari@gmail.com>
2026-09-11 13:37:23 +0700

    make value module

---
+++ slp.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class SLP():
+    def __init__(self):
+        self.
---
+++ value.py
@@ -0,0 +1,120 @@
+import math
+
+
+class Value:
+    def __init__(self, data, _children=()):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda : None
+        self._prev = set(_children)
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self,other))
+
+        def _backward():
+            self.grad = self.grad + out.grad
+            other.grad = other.grad + out.grad
+        out._backward = _backward
+
+        return out
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self,other))
+
+        def _backward():
+            self.grad = self.grad + other.data * out.grad
+            other.grad = other.grad + self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __truediv__(self, other):
+        return self * other ** -1
+
+    def __rtruediv__(self, other):
+        return other * self ** -1
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data ** other, (self,))
+
+        def _backward():
+            self.grad = self.grad + other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def exp(self):
+        out = Value(math.exp(self.data), (self,))
+
+        def _backward():
+            self.grad = self.grad + out.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        t = math.tanh(self.data)
+        out = Value(t, (self,))
+
+        def _backward():
+            self.grad = self.grad + (1 - t ** 2) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def sigmoid(self):
+        s = 1 / (1 + math.exp(-self.data))
+        out = Value(s, (self,))
+
+        def _backward():
+            self.grad = self.grad + s * (1 - s) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        out = Value(self.data if self.data > 0 else 0.0, (self,))
+
+        def _backward():
+            self.grad = self.grad + (1.0 if out.data > 0 else 0.0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
